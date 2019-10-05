d8a08a303d89a26dcbebfbdaff1493e06b3b5e88
Shiya <shiya@keplerfi.com>
2019-10-04 22:36:09 -0400

    hw2

--- hw2/homework/train.py
+++ hw2/homework/train.py
@@ -7,91 +7,95 @@ import torch.nn.functional as F
 
 # From HW1
 class ClassificationLoss(torch.nn.Module):
-    def forward(self, input, target):
-        return F.cross_entropy(input, target)
+	def forward(self, input, target):
+		return F.cross_entropy(input, target)
 
 
 
 def train(args):
-    print('started')
+	print('started')
 
-    from os import path
-    model = CNNClassifier()
-    print('model')
-    train_logger, valid_logger = None, None
-    if args.log_dir is not None:
-        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
-        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'))
+	from os import path
+	model = CNNClassifier()
+	print('model')
+	train_logger, valid_logger = None, None
+	if args.log_dir is not None:
+		train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
+		valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'))
 
-    """
-    Your code here, modify your HW1 code
-    
-    """
+	"""
+	Your code here, modify your HW1 code
+	
+	"""
 
-    learning_rate     = args.learning_rate
-    num_epoch         = args.num_epoch
-    continue_training = args.continue_training
+	device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    model.to(device)
-    if continue_training:
-        from os import path
-        model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'cc.th')))
+	learning_rate     = args.learning_rate
+	num_epoch         = args.num_epoch
+	continue_training = args.continue_training
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
-    loss = ClassificationLoss()
+	model.to(device)
+	if continue_training:
+		from os import path
+		model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'cc.th')))
 
-    train_data = load_data('data/train')
-    valid_data = load_data('data/valid')
+	optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+	loss = ClassificationLoss()
 
-    for epoch in range(num_epoch):
-        model.train()
-        loss_vals, acc_vals, vacc_vals = [], [], []
-        
-        print('Starting epoch', epoch)
-        i = 0
-        for img, label in train_data:
+	train_data = load_data('data/train')
+	valid_data = load_data('data/valid')
 
-            logit = model(img)
-            loss_val = loss(logit, label)
-            acc_val = accuracy(logit, label)
+	for epoch in range(num_epoch):
+		model.train()
+		loss_vals, acc_vals, vacc_vals = [], [], []
+		
+		print('Starting epoch', epoch)
+		i = 0
+		for img, label in train_data:
+			img, label = img.to(device), label.to(device)
 
-            loss_vals.append(loss_val)
-            acc_vals.append(acc_val)
+			logit = model(img)
+			loss_val = loss(logit, label)
+			acc_val = accuracy(logit, label)
 
-            optimizer.zero_grad()
-            loss_val.backward()
-            optimizer.step()
+			loss_vals.append(loss_val.detach().cpu().numpy())
+			acc_vals.append(acc_val.detach().cpu().numpy())
 
-            i += 1
-            train_logger.add_scalar('train/loss', loss_val, epoch+len(train_data) + i)
-            if i % 20 == 0:
-                print('{}: loss={}'.format(i, loss_val))
+			optimizer.zero_grad()
+			loss_val.backward()
+			optimizer.step()
 
-        avg_loss = sum(loss_vals) / len(loss_vals)
-        avg_acc = sum(acc_vals) / len(acc_vals)
+			i += 1
+			train_logger.add_scalar('train/loss', loss_val, epoch+len(train_data) + i)
+			if i % 20 == 0:
+				print('{}: loss={}'.format(i, loss_val))
 
-        model.eval()
-        for img, label in valid_data:
-            vacc_vals.append(accuracy(model(img), label).numpy())
-        avg_vacc = sum(vacc_vals) / len(vacc_vals)
+		avg_loss = sum(loss_vals) / len(loss_vals)
+		avg_acc = sum(acc_vals) / len(acc_vals)
 
-        train_logger.add_scalar('train/accuracy', avg_acc, epoch)
-        valid_logger.add_scalar('train/valid', avg_vacc, epoch)
+		model.eval()
+		for img, label in valid_data:
+			img, label = img.to(device), label.to(device)
+			vacc_vals.append(accuracy(model(img), label).detach().cpu().numpy())
+		avg_vacc = sum(vacc_vals) / len(vacc_vals)
 
-        print('epoch %-3d \t loss = %0.3f \t acc = %0.3f \t val acc = %0.3f' % (epoch, avg_loss, avg_acc, avg_vacc))
-    save_model(model)
+		train_logger.add_scalar('train/accuracy', avg_acc, epoch)
+		valid_logger.add_scalar('train/valid', avg_vacc, epoch)
+
+		print('epoch %-3d \t loss = %0.3f \t acc = %0.3f \t val acc = %0.3f' % (epoch, avg_loss, avg_acc, avg_vacc))
+	save_model(model)
 
 
 if __name__ == '__main__':
-    import argparse
+	import argparse
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--log_dir', default='log')
-    # Put custom arguments here
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--log_dir', default='log')
+	# Put custom arguments here
 
-    parser.add_argument('-n', '--num_epoch', type=int, default=5)
-    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
-    parser.add_argument('-c', '--continue_training', action='store_true')
+	parser.add_argument('-n', '--num_epoch', type=int, default=5)
+	parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
+	parser.add_argument('-c', '--continue_training', action='store_true')
 
-    args = parser.parse_args()
-    train(args)
+	args = parser.parse_args()
+	train(args)
