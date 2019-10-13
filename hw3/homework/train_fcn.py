deb74d21c1b225e876ce0999a3f9151f2d3041e0
Shiya <shiya@keplerfi.com>
2019-10-12 21:37:44 -0400

    hw3

---
+++ hw3/homework/train_fcn.py
@@ -0,0 +1,36 @@
+import torch
+import numpy as np
+
+from .models import FCN, save_model
+from .utils import load_dense_data, DENSE_CLASS_DISTRIBUTION, ConfusionMatrix
+from . import dense_transforms
+import torch.utils.tensorboard as tb
+
+
+def train(args):
+    from os import path
+    model = FCN()
+    train_logger, valid_logger = None, None
+    if args.log_dir is not None:
+        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'), flush_secs=1)
+        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'), flush_secs=1)
+
+    """
+    Your code here, modify your HW1 / HW2 code
+    Hint: Use ConfusionMatrix, ConfusionMatrix.add(logit.argmax(1), label), ConfusionMatrix.iou to compute
+          the overall IoU, where label are the batch labels, and logit are the logits of your classifier.
+    Hint: If you found a good data augmentation parameters for the CNN, use them here too. Use dense_transforms
+    """
+    save_model(model)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--log_dir')
+    # Put custom arguments here
+
+    args = parser.parse_args()
+    train(args)
