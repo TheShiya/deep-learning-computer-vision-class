643b30511a6909ee052bb7f230acd51b4e169f88
Shiya <shiya@keplerfi.com>
2019-10-19 22:50:50 -0400

    hw4

--- hw4/homework/models.py
+++ hw4/homework/models.py
@@ -28,6 +28,17 @@ def extract_peak(heatmap, max_pool_ks=7, min_score=-5, max_det=100):
 
 
 class Detector(torch.nn.Module):
+    class Block(torch.nn.Module):
+        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
+            super().__init__()
+            self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
+                                      stride=stride)
+            self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2)
+            self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2)
+            self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
+
+        def forward(self, x):
+            return F.relu(self.c3(F.relu(self.c2(F.relu(self.c1(x)))))) + self.skip(x)
 	class UpBlock(torch.nn.Module):
 		def __init__(self, n_input, n_output, kernel_size=3, stride=2):
 			super().__init__()
@@ -47,7 +58,7 @@ class Detector(torch.nn.Module):
 		self.n_conv = len(layers)
 		skip_layer_size = [3] + layers[:-1]
 		for i, l in enumerate(layers):
-			self.add_module('conv%d' % i, CNNClassifier.Block(c, l, kernel_size, 2))
+			self.add_module('conv%d' % i, self.Block(c, l, kernel_size, 2))
 			c = l
 		for i, l in list(enumerate(layers))[::-1]:
 			self.add_module('upconv%d' % i, self.UpBlock(c, l, kernel_size, 2))
