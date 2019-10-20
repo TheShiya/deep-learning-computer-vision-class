80a66904f956168cc714c3cd74ef54d8f8ebb6c2
Shiya <shiya@keplerfi.com>
2019-10-19 23:34:29 -0400

    hw4

--- hw4/homework/utils.py
+++ hw4/homework/utils.py
@@ -40,51 +40,6 @@ def load_detection_data(dataset_path, num_workers=0, batch_size=32, **kwargs):
     return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=True)
 
 
-if __name__ == '__main__':
-    dataset = DetectionSuperTuxDataset('dense_data/train')
-    import torchvision.transforms.functional as F
-    from pylab import show, subplots
-    import matplotlib.patches as patches
-    import numpy as np
-
-    fig, axs = subplots(1, 2)
-    for i, ax in enumerate(axs.flat):
-        im, kart, bomb, pickup = dataset[100+i]
-        ax.imshow(F.to_pil_image(im), interpolation=None)
-        for k in kart:
-            ax.add_patch(
-                patches.Rectangle((k[0] - 0.5, k[1] - 0.5), k[2] - k[0], k[3] - k[1], fc='none', ec='r', lw=2))
-        for k in bomb:
-            ax.add_patch(
-                patches.Rectangle((k[0] - 0.5, k[1] - 0.5), k[2] - k[0], k[3] - k[1], fc='none', ec='g', lw=2))
-        for k in pickup:
-            ax.add_patch(
-                patches.Rectangle((k[0] - 0.5, k[1] - 0.5), k[2] - k[0], k[3] - k[1], fc='none', ec='b', lw=2))
-        ax.axis('off')
-    dataset = DetectionSuperTuxDataset('dense_data/train',
-                                       transform=dense_transforms.Compose([dense_transforms.RandomHorizontalFlip(0),
-                                                                           dense_transforms.ToTensor(),
-                                                                           dense_transforms.to_heatmap]))
-    fig.tight_layout()
-    # fig.savefig('box.png', bbox_inches='tight', pad_inches=0, transparent=True)
-
-    fig, axs = subplots(1, 2)
-    for i, ax in enumerate(axs.flat):
-        im, hm, size = dataset[100+i]
-        ax.imshow(F.to_pil_image(im), interpolation=None)
-        hm = hm.numpy().transpose([1, 2, 0])
-        alpha = 0.25*hm.max(axis=2) + 0.75
-        r = 1 - np.maximum(hm[:, :, 1], hm[:, :, 2])
-        g = 1 - np.maximum(hm[:, :, 0], hm[:, :, 2])
-        b = 1 - np.maximum(hm[:, :, 0], hm[:, :, 1])
-        ax.imshow(np.stack((r, g, b, alpha), axis=2), interpolation=None)
-        ax.axis('off')
-    fig.tight_layout()
-    # fig.savefig('heat.png', bbox_inches='tight', pad_inches=0, transparent=True)
-
-    show()
-
-
 class ConfusionMatrix(object):
     def _make(self, preds, labels):
         label_range = torch.arange(self.size, device=preds.device)[None, :]
@@ -132,3 +87,49 @@ class ConfusionMatrix(object):
     @property
     def per_class(self):
         return self.matrix / (self.matrix.sum(1, keepdims=True) + 1e-5)
+
+if __name__ == '__main__':
+    dataset = DetectionSuperTuxDataset('dense_data/train')
+    import torchvision.transforms.functional as F
+    from pylab import show, subplots
+    import matplotlib.patches as patches
+    import numpy as np
+
+    fig, axs = subplots(1, 2)
+    for i, ax in enumerate(axs.flat):
+        im, kart, bomb, pickup = dataset[100+i]
+        ax.imshow(F.to_pil_image(im), interpolation=None)
+        for k in kart:
+            ax.add_patch(
+                patches.Rectangle((k[0] - 0.5, k[1] - 0.5), k[2] - k[0], k[3] - k[1], fc='none', ec='r', lw=2))
+        for k in bomb:
+            ax.add_patch(
+                patches.Rectangle((k[0] - 0.5, k[1] - 0.5), k[2] - k[0], k[3] - k[1], fc='none', ec='g', lw=2))
+        for k in pickup:
+            ax.add_patch(
+                patches.Rectangle((k[0] - 0.5, k[1] - 0.5), k[2] - k[0], k[3] - k[1], fc='none', ec='b', lw=2))
+        ax.axis('off')
+    dataset = DetectionSuperTuxDataset('dense_data/train',
+                                       transform=dense_transforms.Compose([dense_transforms.RandomHorizontalFlip(0),
+                                                                           dense_transforms.ToTensor(),
+                                                                           dense_transforms.to_heatmap]))
+    fig.tight_layout()
+    # fig.savefig('box.png', bbox_inches='tight', pad_inches=0, transparent=True)
+
+    fig, axs = subplots(1, 2)
+    for i, ax in enumerate(axs.flat):
+        im, hm, size = dataset[100+i]
+        ax.imshow(F.to_pil_image(im), interpolation=None)
+        hm = hm.numpy().transpose([1, 2, 0])
+        alpha = 0.25*hm.max(axis=2) + 0.75
+        r = 1 - np.maximum(hm[:, :, 1], hm[:, :, 2])
+        g = 1 - np.maximum(hm[:, :, 0], hm[:, :, 2])
+        b = 1 - np.maximum(hm[:, :, 0], hm[:, :, 1])
+        ax.imshow(np.stack((r, g, b, alpha), axis=2), interpolation=None)
+        ax.axis('off')
+    fig.tight_layout()
+    # fig.savefig('heat.png', bbox_inches='tight', pad_inches=0, transparent=True)
+
+    show()
+
+
