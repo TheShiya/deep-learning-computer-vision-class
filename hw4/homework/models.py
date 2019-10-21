3326dfb37687a69023389e28a2b236c31ba0762e
Shiya <shiya@keplerfi.com>
2019-10-21 01:39:52 -0400

    hw4

--- hw4/homework/models.py
+++ hw4/homework/models.py
@@ -108,6 +108,7 @@ class Detector(torch.nn.Module):
 
 	def __init__(self, layers=[16, 32, 64, 96, 128], n_output_channels=5, kernel_size=3, use_skip=True):
 		super().__init__()
+		self.min_score = min_score
 		self.input_mean = torch.Tensor([0.3521554, 0.30068502, 0.28527516])
 		self.input_std = torch.Tensor([0.18182722, 0.18656468, 0.15938024])
 
