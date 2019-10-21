3dcd610c26ff4eb36fe69a08959727a260402117
Shiya <shiya@keplerfi.com>
2019-10-21 01:32:05 -0400

    hw4

--- hw4/homework/models.py
+++ hw4/homework/models.py
@@ -171,6 +171,7 @@ class Detector(torch.nn.Module):
 			detections = sorted(detections, key=lambda x: x[1])
 			all_detections += detections[-6:]
 
+		all_detections = sorted(all_detections, key=lambda x: x[1])
 		return all_detections[-100:], heatmaps
 
 	def detect_with_size(self, image):
