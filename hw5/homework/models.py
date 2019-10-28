224dd1927e6e2a8dba5ffc36e44a52e5ea7e2902
TheShiya <41601238+TheShiya@users.noreply.github.com>
2019-10-28 14:24:54 -0400

    Update models.py

--- hw5/homework/models.py
+++ hw5/homework/models.py
@@ -104,7 +104,7 @@ class TCN(torch.nn.Module, LanguageModel):
 		for ch in channels:
 			net.append(self.CausalConv1dBlock(in_ch, ch, kernel_size=kernel_size))
 			in_ch = ch * 2
-		net.append(self.Conv1d(in_ch, 28, kernel_size=1))
+		net.append(torch.nn.Conv1d(in_ch, 28, kernel_size=1))
 		self.net = torch.nn.Sequential(*net)
 		self.prob_first = torch.nn.Parameter(torch.ones(1, 28, 1)/28)
 		self.sigmoid = torch.nn.Sigmoid()
