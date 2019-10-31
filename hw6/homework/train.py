0b49d8dfdb96194d99d7aaa99fab2f602662e62b
Shiya <shiya993@gmail.com>
2019-10-31 17:03:21 -0400

    h6

--- hw6/homework/train.py
+++ hw6/homework/train.py
@@ -47,6 +47,8 @@ def train(args):
             img, label = img.to(device).float(), label.to(device).float()
             logit = model(img).float()
 
+            x_label, y_label = label[:,0], label[:,1]
+            x_logit, y_logit = logit[:,0], logit[:,1]
             loss_val = loss(x_logit, x_label) + 0.5*loss(y_logit, y_label)
             train_losses.append(loss_val)
 
