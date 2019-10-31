a7378ff550e4955f277a7d8a9a708320748ba850
Shiya <shiya993@gmail.com>
2019-10-31 14:52:16 -0400

    h6

--- hw6/homework/planner.py
+++ hw6/homework/planner.py
@@ -1,60 +1,121 @@
 import torch
 from .utils import spatial_argmax
+import torch.nn.functional as F
+
 
 
 class Planner(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
+	class Block(torch.nn.Module):
+		def __init__(self, n_input, n_output, kernel_size=3, stride=2):
+			super().__init__()
+			self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
+									  stride=stride)
+			self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2)
+			self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2)
+			self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
 
-        """
-        Your code here
-        """
-        raise NotImplementedError('Planner.__init__')
+		def forward(self, x):
+			return F.relu(self.c3(F.relu(self.c2(F.relu(self.c1(x)))))) + self.skip(x)
+	class UpBlock(torch.nn.Module):
+		def __init__(self, n_input, n_output, kernel_size=3, stride=2):
+			super().__init__()
+			self.c1 = torch.nn.ConvTranspose2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
+									  stride=stride, output_padding=1)
 
-    def forward(self, img):
-        """
-        Your code here
-        Predict the aim point in image coordinate, given the supertuxkart image
-        @img: (B,3,96,128)
-        return (B,2)
-        """
-        raise NotImplementedError("Planner.forward")
+		def forward(self, x):
+			return F.relu(self.c1(x))
+
+	def __init__(self, layers=[16, 32, 64, 96, 128], n_output_channels=1, kernel_size=3, use_skip=True):
+		super().__init__()
+		self.resolution = torch.FloatTensor([128, 96])
+		self.batch_norm = torch.nn.BatchNorm2d(3)
+
+		c = 3
+		self.use_skip = use_skip
+		self.n_conv = len(layers)
+		skip_layer_size = [3] + layers[:-1]
+		for i, l in enumerate(layers):
+			self.add_module('conv%d' % i, self.Block(c, l, kernel_size, 2))
+			c = l
+		for i, l in list(enumerate(layers))[::-1]:
+			self.add_module('upconv%d' % i, self.UpBlock(c, l, kernel_size, 2))
+			c = l
+			if self.use_skip:
+				c += skip_layer_size[i]
+		self.classifier = torch.nn.Conv2d(c, n_output_channels, 1)
+
+	def forward(self, x):
+		z = self.batch_norm(x)
+		up_activation = []
+		for i in range(self.n_conv):
+			# Add all the information required for skip connections
+			up_activation.append(z)
+			z = self._modules['conv%d'%i](z)
+
+		for i in reversed(range(self.n_conv)):
+			z = self._modules['upconv%d'%i](z)
+			# Fix the padding
+			z = z[:, :, :up_activation[i].size(2), :up_activation[i].size(3)]
+			# Add the skip connection
+			if self.use_skip:
+				z = torch.cat([z, up_activation[i]], dim=1)
+		heatmap = self.classifier(z)
+		heatmap = torch.squeeze(heatmap)
+		
+		return (spatial_argmax(heatmap) + 1)/2 * self.resolution
+		
+	# def __init__(self):
+	# 	super().__init__()
+
+	# 	"""
+	# 	Your code here
+	# 	"""
+	# 	raise NotImplementedError('Planner.__init__')
+
+	# def forward(self, img):
+	# 	"""
+	# 	Your code here
+	# 	Predict the aim point in image coordinate, given the supertuxkart image
+	# 	@img: (B,3,96,128)
+	# 	return (B,2)
+	# 	"""
+	# 	raise NotImplementedError("Planner.forward")
 
 
 def save_model(model):
-    from torch import save
-    from os import path
-    if isinstance(model, Planner):
-        return save(model.state_dict(), path.join(path.dirname(path.abspath(__file__)), 'planner.th'))
-    raise ValueError("model type '%s' not supported!" % str(type(model)))
+	from torch import save
+	from os import path
+	if isinstance(model, Planner):
+		return save(model.state_dict(), path.join(path.dirname(path.abspath(__file__)), 'planner.th'))
+	raise ValueError("model type '%s' not supported!" % str(type(model)))
 
 
 def load_model():
-    from torch import load
-    from os import path
-    r = Planner()
-    r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), 'planner.th'), map_location='cpu'))
-    return r
+	from torch import load
+	from os import path
+	r = Planner()
+	r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), 'planner.th'), map_location='cpu'))
+	return r
 
 
 if __name__ == '__main__':
-    from .controller import control
-    from .utils import PyTux
-    from argparse import ArgumentParser
+	from .controller import control
+	from .utils import PyTux
+	from argparse import ArgumentParser
 
 
-    def test_planner(args):
-        # Load model
-        planner = load_model().eval()
-        pytux = PyTux()
-        for t in args.track:
-            steps = pytux.rollout(t, control, planner=planner, max_frames=1000, verbose=args.verbose)
-            print(steps)
-        pytux.close()
+	def test_planner(args):
+		# Load model
+		planner = load_model().eval()
+		pytux = PyTux()
+		for t in args.track:
+			steps = pytux.rollout(t, control, planner=planner, max_frames=1000, verbose=args.verbose)
+			print(steps)
+		pytux.close()
 
 
-    parser = ArgumentParser("Test the planner")
-    parser.add_argument('track', nargs='+')
-    parser.add_argument('-v', '--verbose', action='store_true')
-    args = parser.parse_args()
-    test_planner(args)
+	parser = ArgumentParser("Test the planner")
+	parser.add_argument('track', nargs='+')
+	parser.add_argument('-v', '--verbose', action='store_true')
+	args = parser.parse_args()
+	test_planner(args)
--- hw6/homework/train.py
+++ hw6/homework/train.py
@@ -16,8 +16,72 @@ def train(args):
     Your code here, modify your HW4 code
     
     """
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    save_model(model)
+    if args.continue_training:
+        model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'fcn.th')))
+        global_step = pickle.load(open('global_step.p', 'rb'))
+    else:
+        global_step = 0
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=1e-5)
+    #loss = torch.nn.BCEWithLogitsLoss().to(device) #weight=w
+    loss = torch.nn.MSELoss(reduction='mean')
+    transform = dense_transforms.Compose([
+        dense_transforms.ColorJitter(0.9, 0.9, 0.7, 0.1),
+        dense_transforms.RandomHorizontalFlip(),
+        dense_transforms.ToTensor(),
+        ])
+    
+    train_data = load_data('drive_data/', transform=transform)
+    valid_data = load_data('drive_data/', transform=dense_transforms.ToTensor())
+
+    model = model.to(device)
+    for epoch in range(args.num_epoch):
+        model.train()
+        train_losses = []        
+        for img, label in train_data:
+            img, label = img.to(device).float(), label.to(device).float()
+            logit = model(img).float()
+            
+            loss_val = loss(logit, label)
+            train_losses.append(loss_val)
+
+            if epoch > 0 and train_logger is not None:
+                train_logger.add_scalar('loss', loss_val, global_step)
+
+            if global_step % 100 == 0:
+                print('{}: loss: {}'.format(global_step, loss_val))
+
+            optimizer.zero_grad()
+            loss_val.backward()
+            optimizer.step()
+            global_step += 1
+
+        model.eval()
+        valid_losses = []
+        count = 0
+        # for img, label, square in valid_data:
+        #     if count == 20:
+        #         break           
+        #     img, label = img.to(device).float(), label.to(device).float()
+        #     logit = model(img).float()
+        #     valid_loss = loss(logit.permute((0,2,3,1)), label.permute((0,2,3,1)))
+        #     valid_losses.append(valid_loss)
+        #     count += 0          
+        
+        avg_train_loss = sum(train_losses) / len(train_losses)
+        #avg_valid_loss = sum(valid_losses) / len(valid_losses)
+
+        if valid_logger is None or train_logger is None:
+            train_logger.add_scalar('avg_loss', avg_train_loss, epoch)
+            #valid_logger.add_scalar('avg_loss', avg_valid_loss, epoch)
+
+        print('epoch %-3d \t train = %0.3f \t valid = %0.3f \t' % (epoch, avg_train_loss, avg_train_loss))
+        
+        pickle.dump(global_step, open('global_step.p', 'wb'))
+        save_model(model, suffix=str(epoch))
+    save_model(model, suffix='')
 
 
 if __name__ == '__main__':
