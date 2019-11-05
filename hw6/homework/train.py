b0ae52aba8f1277b0ac787b68a11bcaee602e14a
Shiya <shiya@keplerfi.com>
2019-11-04 21:28:42 -0500

    hw6

--- hw6/homework/train.py
+++ hw6/homework/train.py
@@ -56,8 +56,8 @@ def train(args):
                 train_logger.add_scalar('loss', loss_val, global_step)
 
             if global_step % 10 == 0:
-                print('{}: loss: {}, max loss: {}'.format(global_step,
-                    round(loss_val**(1/power), 2), round(max_loss**(1/power), 2)))
+                print('{}: loss: {:.2f}, max loss: {:.2f}'.format(global_step,
+                    loss_val**(1/power), 2, max_loss**(1/power), 2))
 
             optimizer.zero_grad()
             loss_val.backward()
